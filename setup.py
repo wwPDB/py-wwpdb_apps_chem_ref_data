1afc02a4f736175f83a6cbea7d5b847a4023c276
Ezra Peisach <ezra.peisach@rcsb.org>
2019-10-28 13:29:14 -0400

    v0.6

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     #
     install_requires=['wwpdb.utils.config', 'wwpdb.utils.db ~= 0.10',
                       'wwpdb.utils.session ~= 0.4', 'wwpdb.io >= 0.2',
-                      'mmcif.utils ~= 0.18.dev2', 'wwpdb.utils.oe_util', 'jellyfish',
-                      'wwpdb.utils.cc_dict_util', 'wwpdb.utils.dp ~= 0.19.dev1',
+                      'mmcif.utils ~= 0.18', 'wwpdb.utils.oe_util', 'jellyfish',
+                      'wwpdb.utils.cc_dict_util', 'wwpdb.utils.dp ~= 0.19',
                       'rcsb.utils.multiproc', 'wwpdb.utils.ws_utils'],
     packages=find_packages(exclude=['wwpdb.apps.tests-chem_ref_data', 'mock-data']),
     # Enables Manifest to be used
